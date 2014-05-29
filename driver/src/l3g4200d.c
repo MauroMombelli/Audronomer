659a5a474eb846f21289248ac2f23c926f192c5b
lestofante <mombelli.mauro@gmail.com>
2014-05-30 00:36:56 +0200

    all sensors seems to work, need to have a look on they vector,especially magne seems out-of-scale

--- driver/src/l3g4200d.c
+++ driver/src/l3g4200d.c
@@ -18,19 +18,21 @@ static const SPIConfig spi1cfg = {
 };
 
 msg_t l3g4200d_init(void) {
-	char txbuf[2] = { 0x20, 0xcF };
+	char txbuf[2] = { 0x20, 0xbF };
 	char rxbuf[2];
 
+	spiAcquireBus(&SPID1);
 	spiStart(&SPID1, &spi1cfg);
-
 	spiSelect(&SPID1);
-	spiExchange(&SPID1, 2, txbuf, rxbuf);
-	txbuf[0] = 0x23;
-	txbuf[1] = 0xD0;
-	spiExchange(&SPID1, 2, txbuf, rxbuf);
-	spiUnselect(&SPID1);
 
+	spiExchange(&SPID1, 2, txbuf, rxbuf);
+	txbuf[0] = 0xc0 | 0x20;
+	txbuf[1] = 0xFF;
+	spiExchange(&SPID1, 2, txbuf, rxbuf);
+
+	spiUnselect(&SPID1);
 	spiStop(&SPID1);
+	spiReleaseBus(&SPID1);
 
 	return rxbuf[1];
 }
@@ -38,24 +40,38 @@ msg_t l3g4200d_init(void) {
 msg_t read_gyroscope(int16_t *values) {
 	/* read from L3GD20 registers and assemble data */
 	/* 0xc0 sets read and address increment */
+/*
+	char txbuf2[24] = {  0xFF }; //all cells to 0xff
+	uint8_t i;
+	for (i=0; i< 24; i++){
+		txbuf2[i] = 0xFF;
+	}
+	txbuf2[24] = 0xc0 | 0x20; //read register, autoincrement
+	char rxbuf2[24];*/
+
+
 	char txbuf[8] = { 0xc0 | 0x27, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
 	char rxbuf[8];
 
+	spiAcquireBus(&SPID1);
 	spiStart(&SPID1, &spi1cfg);
-
 	spiSelect(&SPID1);
-	spiExchange(&SPID1, 8, txbuf, rxbuf);
-	spiUnselect(&SPID1);
 
+	//spiExchange(&SPID1, 24, txbuf2, rxbuf2);
+
+	spiExchange(&SPID1, 8, txbuf, rxbuf);
+
+	spiUnselect(&SPID1);
 	spiStop(&SPID1);
+	spiReleaseBus(&SPID1);
 
 	if ( (rxbuf[1] & 0x8) || (rxbuf[1] & 0x7) ) {
-		values[0] = ((int16_t)rxbuf[3] << 8) | rxbuf[2];
-		values[1] = ((int16_t)rxbuf[5] << 8) | rxbuf[4];
-		values[2] = ((int16_t)rxbuf[7] << 8) | rxbuf[6];
+		values[0] = ((uint16_t)rxbuf[3] << 8) | rxbuf[2];
+		values[1] = ((uint16_t)rxbuf[5] << 8) | rxbuf[4];
+		values[2] = ((uint16_t)rxbuf[7] << 8) | rxbuf[6];
 		return RDY_OK;
 	}else{
 		values[0] = values[1] = values[2] = 0;
-		return RDY_RESET;
+		return RDY_TIMEOUT;
 	}
 }
--- driver/src/lsm303dlh.c
+++ driver/src/lsm303dlh.c
@@ -17,16 +17,20 @@ static const int LSM_ADDR_ACC = 0x19;
 static const int LSM_ADDR_MAG = 0x1E;
 
 msg_t lsm303dlh_init(void) {
-	uint8_t buffer_tx[] = { 0x20, 0x77, 0x21, 0x9C, 0x23, 0xc8, };
+	//acc
+	uint8_t buffer_tx[] = { 0x20, 0x77, 0x21, 0x90, 0x23, 0xc8, };
+
+	//magne
 	uint8_t buffer_tx_mag[] = { 0x00, 0x18, 0x01, 0xE0, 0x02, 0x00 };
 
 	systime_t tmo = MS2ST(4);
 
 	uint8_t i;
 
-	i2cStart(&I2CD1, &i2cconfig);
+
 
 	i2cAcquireBus(&I2CD1);
+	i2cStart(&I2CD1, &i2cconfig);
 
 	//Configure Accelerometer
 	for (i = 0; i < sizeof(buffer_tx) / 2; i++) {
@@ -36,10 +40,10 @@ msg_t lsm303dlh_init(void) {
 	for (i = 0; i < sizeof(buffer_tx_mag) / 2; i++) {
 		i2cMasterTransmitTimeout(&I2CD1, LSM_ADDR_MAG, buffer_tx_mag + i * 2, 2, NULL, 0, tmo);
 	}
-
+	i2cStop(&I2CD1);
 	i2cReleaseBus(&I2CD1);
 
-	i2cStop(&I2CD1);
+
 
 	return RDY_OK;
 }
@@ -53,19 +57,21 @@ msg_t read_acceleration(int16_t *values) {
 
 	systime_t tmo = MS2ST(4);
 
-	i2cStart(&I2CD1, &i2cconfig);
+
 
 	i2cAcquireBus(&I2CD1);
-	status = i2cMasterTransmitTimeout(&I2CD1, LSM_ADDR_ACC, &buffer_tx, 1, buffer_rx, 7, tmo);
+	i2cStart(&I2CD1, &i2cconfig);
+	status = i2cMasterTransmitTimeout(&I2CD1, LSM_ADDR_ACC, &buffer_tx, 1, buffer_rx, sizeof(buffer_rx), tmo); //please, sizeof works only on array, never use with pointer, even if pointing array
+	i2cStop(&I2CD1);
 	i2cReleaseBus(&I2CD1);
 
-	i2cStop(&I2CD1);
+
 
 	//buffer_rx[0] contains data ready bit
 	if ( status == RDY_OK && (buffer_rx[0] & 0x8) ){
-		values[0] = ((int16_t)buffer_rx[1]<<8)|buffer_rx[2];
-		values[1] = ((int16_t)buffer_rx[3]<<8)|buffer_rx[4];
-		values[2] = ((int16_t)buffer_rx[5]<<8)|buffer_rx[6];
+		values[0] = ((int16_t)((uint16_t)buffer_rx[1] << 8) + buffer_rx[2]);
+		values[1] = ((int16_t)((uint16_t)buffer_rx[3] << 8) + buffer_rx[4]);
+		values[2] = ((int16_t)((uint16_t)buffer_rx[5] << 8) + buffer_rx[6]);
 	}else{
 		values[0] = values[1] = values[2] = 0;
 	}
@@ -90,9 +96,9 @@ msg_t read_magetometer(int16_t *values) {
 
 	//buffer_rx[6] ontains data ready bit
 	if ( status == RDY_OK /*&& (buffer_rx[6] & 0x1) */){//does not seems to work
-		values[0] = ((int16_t)buffer_rx[0] << 8) | buffer_rx[1];
-		values[2] = ((int16_t)buffer_rx[2] << 8) | buffer_rx[3];
-		values[1] = ((int16_t)buffer_rx[4] << 8) | buffer_rx[5];
+		values[0] = ((int16_t)((uint16_t)buffer_rx[0] << 8) + buffer_rx[1]);
+		values[2] = ((int16_t)((uint16_t)buffer_rx[2] << 8) + buffer_rx[3]);
+		values[1] = ((int16_t)((uint16_t)buffer_rx[4] << 8) + buffer_rx[5]);
 	}else{
 		values[0] = values[1] = values[2] = 0;
 	}
